81645d5f418b384a2791ba59ae8eedafa7812dba
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-06-14 18:19:55 +0800

    Added the NextJS Analytics library

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Analytics } from "@vercel/analytics/react";
 
 import "../globals.css";
 import LeftSidebar from "@/components/shared/LeftSidebar";
@@ -42,6 +43,7 @@ export default function RootLayout({
           </main>
 
           <Bottombar />
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
