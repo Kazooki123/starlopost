8fe03066b5d885b4148c0a6ab07e01e7480c55a9
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-06-14 12:14:18 +0800

    Changed the logo name

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,8 +13,8 @@ import Topbar from "@/components/shared/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Threads",
-  description: "A Next.js 13 Meta Threads application",
+  title: "StarloPost",
+  description: "A Next.js 13 Meta Threads Clone application",
 };
 
 export default function RootLayout({
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -8,7 +8,7 @@ function Topbar() {
     <nav className='topbar'>
       <Link href='/' className='flex items-center gap-4'>
         <Image src='/logo.svg' alt='logo' width={28} height={28} />
-        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+        <p className='text-heading3-bold text-light-1 max-xs:hidden'>StarloPost</p>
       </Link>
 
       <div className='flex items-center gap-1'>
