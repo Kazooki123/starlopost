025181ae7e519a1386eb305651bbc51e6666bb77
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-07-09 11:45:03 +0800

    feat:(Added 'analytics/' route for testing, will change later on)

---
+++ app/analytics/page.tsx
@@ -0,0 +1,9 @@
+
+// import BarCharts from '@/components/charts/BarCharts';
+import LineCharts from '@/components/charts/LineChart';
+
+export default function Page() {
+    return (
+        <LineCharts />
+    )
+}
