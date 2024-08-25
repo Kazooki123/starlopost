56de2fb2a9ea8bd5a31f81325a6e69e46fb0b299
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-08-25 17:54:06 +0800

    Added games for the '/games' route upcoming sooner, big updates. all games are not made by me they're made by other professional JS game developers, Thank them instead.

---
+++ app/games/layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Inter } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import '../globals.css'
+import { Analytics } from "@vercel/analytics/react";
+
+const fontHeading = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-heading',
+})
+
+const fontBody = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-body',
+})
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={cn("antialiased", fontHeading.variable, fontBody.variable)}
+      >
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
