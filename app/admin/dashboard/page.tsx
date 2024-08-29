59916ab0a6d87a77aa188a8bbee67e88104ae6dc
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-08-29 18:09:34 +0800

    modified and added layouts in the admin route

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -20,10 +20,10 @@ export default async function AdminDashboard() {
       <h1>Admin Dashboard</h1>
       
       <p>Hello fellow moderator! Welcome to the moderation dashboard</p>
-      <p>Here, you can ban, kick or restrict someone's account if they post something that doesn't align with our websites rules or TOS.</p>
+      <p>Here, you can ban, kick or restrict someones account if they post something that does not align with our websites rules or TOS.</p>
       
       <h3>And oh!</h3>
-      <p>Just be reminded that you can be removed as an admin or moderator of the website base on how you take care of the website. Mods don't actually have the full gain access of the website like the database, keys, private keys, etc.</p>
+      <p>Just be reminded that you can be removed as an admin or moderator of the website base on how you take care of the website. Mods do not actually have the full gain access of the website like the database, keys, private keys, etc.</p>
     </div>
   );
 }
