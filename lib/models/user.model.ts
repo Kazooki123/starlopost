aae5abdd165f690bdf246de6a0c6071b45e08252
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-08-31 18:44:23 +0800

    fixing errors of starlopost

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -5,10 +5,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  admin: {
-    type: Boolean,
-    default: false,
-  },
   username: {
     type: String,
     unique: true,
