3b2b85382df013a4e3b87e6dcdb60ac105fbcfaf
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-08-31 17:34:02 +0800

    changes on api/hello/hello.ts

--- app/api/hello/hello.ts
+++ app/api/hello/hello.ts
@@ -1,11 +1,25 @@
 /* eslint-disable camelcase */
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 import {
   initializeWasm,
   hello_response,
 } from "../../../lib/rust_wasm/hello_module";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    // Initialize the WebAssembly module
     await initializeWasm();
-    await hello_response();
+
+    // Get the response from the Rust function
+    const response = await hello_response();
+
+    // Send the response back to the client
+    res.status(200).json(JSON.parse(response));
+  } catch (error) {
+    // Handle errors and send a 500 status code with the error message
+    res.status(500).json({ error: error });
+  }
 }
